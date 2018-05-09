70c5dff07cbb7b34f5a977644bc1bb47dd6c3409
David Arellano <dtarellano1@gmail.com>
2018-05-09 14:38:28 -0700

    added nav component

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,40 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        landing
-      </h1>
-      <h2 class="subtitle">
-        Building a Lanidn from Scratch
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <div>
+    <app-nav/>
+    <section class="container">
+      <div>
+        <app-logo/>
+        <h1 class="title">
+          landing
+        </h1>
+        <h2 class="subtitle">
+          Building a Lanidn from Scratch
+        </h2>
+        <div class="links">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            class="button--green">Documentation</a>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            class="button--grey">GitHub</a>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from '~/components/AppLogo.vue';
+import Nav from '~/components/Nav.vue';
 
 export default {
   components: {
-    AppLogo
+    AppLogo,
+    'app-nav': Nav
   }
-}
+};
 </script>
 
 <style>
@@ -42,7 +47,8 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
