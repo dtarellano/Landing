89aabbe72123b18ccde18c0f3d6251cf3ec2ddf5
David Arellano <dtarellano1@gmail.com>
2018-05-09 14:28:59 -0700

    added bulma to module config

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,16 +3,15 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'landing',
+    title: 'home',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Building a Lanidn from Scratch' }
+      { hid: 'description', name: 'description', content: 'home page for tipe' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
+  modules: ['@nuxtjs/bulma', '@nuxtjs/font-awesome'],
   /*
   ** Customize the progress bar color
   */
@@ -21,18 +20,24 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    /*
+    postcss: {
+      plugins: {
+        'postcss-custom-properties': {
+          warnings: false
+        }
+      }
+    } /*
     ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
+    */,
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
+};
