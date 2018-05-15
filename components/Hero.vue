0e54287f44a4ac8d2fbda40361f4de4a7957cb8a
David Arellano <dtarellano1@gmail.com>
2018-05-15 14:17:08 -0700

    displaying correct content

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,6 +1,26 @@
 <template>
-  <div class="container">
-    <h1 class="Title">Hero Component</h1>
+  <div class="container is-fluid">
+    <div class="columns">
+      <div class="column">
+        <div class="card-content">
+          <div class="media-content">
+            <p class="title is-4">Stop Waisting Food.</p>
+          </div>
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </div>
+          <button class="button is-primary">Free Trial</button>
+          <button class="button">Learn More</button>
+        </div>
+      </div>
+      <div class="column">
+          <div class="card-image">
+            <figure class="image">
+              <img src="http://via.placeholder.com/350x200" alt="Intro Pic">
+            </figure>
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 
