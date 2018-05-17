ac41f43054c9269b4563de9ee959783d274dbadc
David Arellano <dtarellano1@gmail.com>
2018-05-17 14:37:51 -0700

    removed boilerplate styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="body">
     <app-nav />
     <app-hero />
   </div>
@@ -20,31 +20,34 @@ export default {
 </script>
 
 <style>
-.container {
+.body {
+  background-color: #f6fbfd;
+}
+/* .container {
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
+} */
 
-.title {
+/* .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; /* 1 */
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
-}
+} */
 
-.subtitle {
+/* .subtitle {
   font-weight: 300;
   font-size: 42px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
+} */
 
 .links {
   padding-top: 15px;
